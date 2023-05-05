fdb7856d2b14bb5713ff31320a00ee50b9e82456
imskanand <the.anand3099@gmail.com>
2023-05-05 11:34:20 +0530

    LaLaLa: Nothing fNew Just some style changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="bg-green-300 p-2 rounded-md">Campus Pick</p>
+    <main className="bg-gray-700 flex min-h-screen flex-col items-center justify-between p-24">
+      <Link
+        href="https://campuspick.vercel.app"
+        className="bg-green-500 p-2 rounded-md font-bold hover:bg-green-700 hover:scale-105"
+      >
+        Campus Pick
+      </Link>
     </main>
   );
 }
